8126755b3e067e9b8f488107e6a87772ca43abf9
toduko <teddyepich@gmail.com>
2021-03-01 14:36:49 +0200

    Functions Have One Exit

--- ds_interface.c
+++ ds_interface.c
@@ -3,84 +3,97 @@
 
 DSError DS_ReadInt(const DSID id, S32 *value)
 {
+  DSError status = SUCCESS;
   if (id >= DATA_SIZE)
   {
     Log_Result(__FUNCTION__, OUT_OF_BOUNDS);
-    return OUT_OF_BOUNDS;
-  }
-  S32 *ptr = (S32 *)DS_GENERATED_DATA[id].data;
-  if (ptr)
-  {
-    *value = *ptr;
+    status = OUT_OF_BOUNDS;
   }
   else
   {
-    Log_Result(__FUNCTION__, POINTER_ERROR);
-    return POINTER_ERROR;
+    S32 *ptr = (S32 *)DS_GENERATED_DATA[id].data;
+    if (ptr)
+    {
+      *value = *ptr;
+      Log_Result(__FUNCTION__, SUCCESS);
+    }
+    else
+    {
+      Log_Result(__FUNCTION__, POINTER_ERROR);
+      status = POINTER_ERROR;
+    }
   }
-  Log_Result(__FUNCTION__, SUCCESS);
-  return SUCCESS;
+  return status;
 }
 
 DSError DS_ReadString(const DSID id, char *buff, const U32 BuffSize)
 {
+  DSError status = SUCCESS;
   if (id >= DATA_SIZE)
   {
     Log_Result(__FUNCTION__, OUT_OF_BOUNDS);
-    return OUT_OF_BOUNDS;
-  }
-  char *ptr = (char *)DS_GENERATED_DATA[id].data;
-  if (ptr)
-  {
-    snprintf(buff, BuffSize, "%s", ptr);
+    status = OUT_OF_BOUNDS;
   }
   else
   {
-    snprintf(buff, BuffSize, "%s", "");
-    Log_Result(__FUNCTION__, POINTER_ERROR);
-    return POINTER_ERROR;
+    char *ptr = (char *)DS_GENERATED_DATA[id].data;
+    if (ptr)
+    {
+      snprintf(buff, BuffSize, "%s", ptr);
+      Log_Result(__FUNCTION__, SUCCESS);
+    }
+    else
+    {
+      snprintf(buff, BuffSize, "%s", "");
+      Log_Result(__FUNCTION__, POINTER_ERROR);
+      status = POINTER_ERROR;
+    }
   }
-  Log_Result(__FUNCTION__, SUCCESS);
-  return SUCCESS;
+  return status;
 }
 
 DSError DS_WriteInt(const DSID id, const S32 value)
 {
+  DSError status = SUCCESS;
   if (id >= DATA_SIZE)
   {
     Log_Result(__FUNCTION__, OUT_OF_BOUNDS);
-    return OUT_OF_BOUNDS;
+    status = OUT_OF_BOUNDS;
   }
   S32 *ptr = (S32 *)DS_GENERATED_DATA[id].data;
   if (ptr)
   {
     *ptr = value;
+    Log_Result(__FUNCTION__, SUCCESS);
   }
   else
   {
     Log_Result(__FUNCTION__, POINTER_ERROR);
-    return POINTER_ERROR;
+    status = POINTER_ERROR;
   }
-  Log_Result(__FUNCTION__, SUCCESS);
-  return SUCCESS;
+
+  return status;
 }
 
 DSError DS_WriteString(const DSID id, char *string)
 {
+  DSError status = SUCCESS;
   if (id >= DATA_SIZE)
   {
     printf("DS_WriteString result: OUT_OF_BOUNDS\n");
-    return OUT_OF_BOUNDS;
+    status = OUT_OF_BOUNDS;
   }
   char *ptr = (char *)DS_GENERATED_DATA[id].data;
   if (ptr)
-    snprintf(ptr, sizeof(*ptr), "%s", string);
+  {
+    snprintf(ptr, sizeof(string), "%s", string);
+    Log_Result(__FUNCTION__, SUCCESS);
+  }
   else
   {
     Log_Result(__FUNCTION__, POINTER_ERROR);
-    return POINTER_ERROR;
+    status = POINTER_ERROR;
   }
-  DS_GENERATED_DATA[id].data = string;
-  Log_Result(__FUNCTION__, SUCCESS);
-  return SUCCESS;
+
+  return status;
 }
